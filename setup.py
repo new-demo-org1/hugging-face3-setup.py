0fe042a662464a1c0c504adbf0a0cc4facdc0474
AkBacancy <80810815+AkBacancy@users.noreply.github.com>
2024-02-09 11:07:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ setup(
     version='1.0.0',
     packages=find_packages(),  # Automatically find all packages
     install_requires=[
-        'xlm-roberta-large==1.2',
-        '007J/smile',
-        'xlnet-base-cased',
-        'xlnet-large-cased'
+        "xlm-roberta-large==4.13.0",
+        "09panesara/distilbert-base-uncased-finetuned-cola",
+        "xlnet-base-cased",
+        "xlnet-large-cased",
+        "007J/smile==1.235.dd",
+        "0307061430/xuangou",
+        "0x7194633/keyt5-base",
+        "0x7194633/keyt5-large",
+        "0xDEADBEA7/DialoGPT-small-rick",
+        "123123/ghfk"
     ],
     author='Your Name',
     author_email='your@email.com',
