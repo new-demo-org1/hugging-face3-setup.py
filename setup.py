c268a20dadcecc191c24245f143975340e5af4db
AkBacancy <80810815+AkBacancy@users.noreply.github.com>
2024-02-15 18:45:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,11 @@ setup(
     version='1.0.0',
     packages=find_packages(),  # Automatically find all packages
     install_requires=[
-        "xlm-roberta-large==4.13.0",
-        "09panesara/distilbert-base-uncased-finetuned-cola",
-        "xlnet-base-cased",
-        "xlnet-large-cased",
-        "007J/smile==1.235.dd",
+        "0x7194633/keyt5-base==4.13.0",
         "0307061430/xuangou",
-        "0x7194633/keyt5-base",
-        "0x7194633/keyt5-large",
-        "0xDEADBEA7/DialoGPT-small-rick",
-        "123123/ghfk"
+        "Salesforce/ctrl",
+        "007J/smile==1.235.dd",
+        "albert-base-v1"
     ],
     author='Your Name',
     author_email='your@email.com',
